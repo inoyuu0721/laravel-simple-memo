d4b5e7c31bb69c3767d626c0960ef6e9d74cf785
Inoue Yuichi <77316336+inoyuu0721@users.noreply.github.com>
2021-07-21 02:19:39 +0900

    Heroku

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -11,15 +11,14 @@
         @error('content')
             <div class="alert alert-danger">メモ内容を入力してください！</div>
         @enderror
-        @foreach($tags as $t)
-        <div class="form-check form-check-inline mb-3">
-            <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $t['id']}}" value="{{ $t['id']}}">
-            <label class="form-check-label" for="{{ $t['id']}}">{{ $t['name'] }}</label>
-        </div>
-        @endforeach
-            <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力"/> 
-            <button type="submit" class="btn btn-primary">保存</button>
-        </div>
+    @foreach($tags as $t)
+    <div class="form-check form-check-inline mb-3">
+        <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $t['id']}}" value="{{ $t['id']}}">
+        <label class="form-check-label" for="{{ $t['id']}}">{{ $t['name'] }}</label>
+    </div>
+    @endforeach
+        <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力"/> 
+        <button type="submit" class="btn btn-primary">保存</button>
     </form>
 </div>
 @endsection
